d713f0c3d3b0e9110e9f9389e11764185aa00041
Lanz Bahinting <lorenzo@itmaskinen.se>
2021-02-04 02:53:47 +0800

    Update wage api endpoint.

---
+++ Core/Dtos/Wages/Input/UpdateWageInputDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Dtos.Wages.Input
+{
+    public class UpdateWageInputDto
+    {
+        public int Id { get; set; }
+        public string SdrType { get; set; }
+        public string CampaignType { get; set; }
+        public decimal BasePay { get; set; }
+        public decimal IncentivePay { get; set; }
+    }
+}
